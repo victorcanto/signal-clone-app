2001c9008db64ca7865c5f29b143b5cc7212908f
Victor Canto <iamvictorcanto@gmail.com>
2022-10-16 16:10:08 -0300

    chore: initial setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    root: ["./src"],
+                    extensions: [
+                        ".ios.js",
+                        ".android.js",
+                        ".js",
+                        ".ts",
+                        ".tsx",
+                        ".json",
+                    ],
+                    alias: {
+                        "@components": "./src/components",
+                        "@screens": "./src/screens",
+                        "@utils": "./src/utils",
+                    },
+                },
+            ],
+        ],
+    };
 };
